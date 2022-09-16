d9d3e72c357883d472f802f61bf9d43f828e8507
Marko Glavic <glavim@gmail.com>
2022-09-16 15:21:36 +0100

    Nested objects.

--- fundamentals.js
+++ fundamentals.js
@@ -1,7 +1,11 @@
 const aCar = {
     owner : "Joe Bloggs",
-    type : 'Toyota Corolla 1.8',
-    registration : '201WD1058'
+    type : {model : "Corolla",
+  make: "Toyota",
+CC:1.8},
+    registration :{   year: 201,code:"WD",number:1058}
   };
   
   console.log(aCar.owner);
+  console.log(aCar.owner + ' drives a ' + aCar.type.make)
+  console.log("Reg. = "+ aCar.registration.year + "-"+ aCar.registration.code + "-" + aCar.registration.number)
