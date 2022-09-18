0e3dbf1ce3e22a91f7fbfa6734700036d14874c2
Marko Glavic <glavim@gmail.com>
2022-09-18 17:33:46 +0100

    Arrays

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,10 @@
 const aCar = {
   owner: "Joe Bloggs",
+  address : '3 Walkers Lane',
+  previous_owners: [
+    {name: "Pat Smith", address: "1 Main Street"},
+    {name: "Sheila Dwyer", address: "2 High Street"}
+  ],
   type: {
     model: "Corolla",
     make: "Toyota",
@@ -19,3 +24,4 @@ console.log(aCar.owner);
 console.log(aCar.owner + ' drives a ' + aCar.type.make)
 console.log("Reg. = " + aCar.registration.year + "-" + aCar.registration.code + "-" + aCar.registration.number)
 console.log("It is a " + aCar.colour.exterior + " car, " + aCar.mileage + " mileage, " + "with " + aCar.colour.interior.texture + " interior.")
+console.log("First owner : " + aCar.previous_owners[0].name + "  - " + aCar.previous_owners[0].address)
